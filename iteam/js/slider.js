56f17d9b27428e6414d729d79c3eb983ef21ebd4
Eugene <j.gashuck@gmail.com>
2019-10-31 23:54:47 +0200

    scroll

--- iteam/js/slider.js
+++ iteam/js/slider.js
@@ -7,31 +7,51 @@ function getTheColor( colorVal ) {
               myRed = 255;
               myGreen = 255;
               third = 240; 
+              border="rgb(255, 0, 0)"
               first="none";
               pos="none"
+            //   bord="block"
+            firstBorderColor="rgb(214, 221, 234)"
+            secondBorderColor="rgb(214, 221, 234)"
+
         }
     else if (val[1] < 555){
             myRed = 255;
             myGreen = 0;
             third= 0;
+            border="rgb(255, 0, 0)"
             first="block";
-            pos="none"
+            pos="none";
+            firstBorderColor="rgb(255, 0, 0)"
+            secondBorderColor="rgb(214, 221, 234)"
+            
+        // bord="block"
     }
-    else if(val[1] < 882.5)
+    else if(val[1] < 832.5)
     {
             myRed = 0;
             myGreen = 0;
             third = 0; 
+            border="rgb(0,0,0)"
             first="block";
-            pos="block"       
+            pos="block"
+            
+            firstBorderColor="rgb(0,0,0)"
+            secondBorderColor="rgb(0,0,0)"
+            
+        // bord="block"       
     }
     else
     {
         myRed = 223;
         myGreen = 220;
         third= 217
+        border="rgb(223,220,214)"
         first="block";
-        pos="block"  
+        pos="block";
+        firstBorderColor="rgb(223,220,214)"
+        secondBorderColor="rgb(223,220,214)"
+        // bord="block" 
     }
   
     theColor = "rgb(" + myRed + "," + myGreen +","+ third + ")"; 
@@ -41,12 +61,14 @@ function getTheColor( colorVal ) {
 function refreshSwatch() {
   var slider = $( "#slider" ).slider( "values" ),
   myColor = getTheColor( slider );
-  
+//   $( "#slider .slider__bord" ).css( "display", bord );
   $( "#slider .ui-slider-range" ).css( "background-color", myColor );
-  $( "#slider .ui-state-default, .ui-widget-content .ui-state-default" ).css( "border-color", myColor );
+  $( "#slider .ui-state-default, .ui-widget-content .ui-state-default" ).css( "border-color", border );
+  $( "#slider .slider__bord" ).css( "background", firstBorderColor );
+  $( "#slider  .slider__secBord" ).css( "background", secondBorderColor );
   $( "#slider .statuses__sec" ).css( "display", pos );
-  
   $( "#slider .statuses__first" ).css( "display", first );
+  
   
     var val = $('#slider').slider("values");
     console.log(val[1]);
