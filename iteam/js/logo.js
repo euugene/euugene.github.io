12e046cff8d47f34092fcca411ad86ad0f469d26
Eugene <j.gashuck@gmail.com>
2019-10-31 18:26:26 +0200

    1

--- iteam/js/logo.js
+++ iteam/js/logo.js
@@ -6,23 +6,26 @@
 //     slidesToShow: 3,
 //     slidesToScroll: 1
 //   });
-$('.slide').slick({
-  dots: true,
-  infinite: false,
-  speed: 300,
-  slidesToShow: 4,
-  slidesToScroll: 4,
-  responsive: [
-    {
-      breakpoint: 6000,
-      settings: "unslick"
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
-      }
-    }
-  ]
-})
+
+
+
+// $('.slide').slick({
+//   dots: true,
+//   infinite: false,
+//   speed: 300,
+//   slidesToShow: 4,
+//   slidesToScroll: 4,
+//   responsive: [
+//     {
+//       breakpoint: 6000,
+//       settings: "unslick"
+//     },
+//     {
+//       breakpoint: 600,
+//       settings: {
+//         slidesToShow: 2,
+//         slidesToScroll: 2
+//       }
+//     }
+//   ]
+// })
--- iteam/js/slider.js
+++ iteam/js/slider.js
@@ -1,74 +1,52 @@
-// $("#slider-range").slider({
-//   range: "min",
-//   min: 1,
-//   max: 500,
-//   value: 37,
-//   slide: function( event, ui ) {
-//     $( "#amount" ).val( "$" + ui.value );
-//   }
-// });
-// $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "value" ));
-
-// $( "#polzunok" ).slider({
-//     animate: "slow",
-//     range: "min",    
-//     value: 50
-// });
-// jQuery("#slider").slider({
-// 	min: 0,
-// 	max: 1000,
-// 	values: [0,1],
-// 	range: true
-    
-// });
+// if (val[i] < itemWidth * i + 1);
 function getTheColor( colorVal ) {
     var theColor = ""; 
      var val = colorVal;
      var myRed,myGreen,third;
-    // if (val[1] < 277.5) {
-    //           myRed = 255;
-    //           myGreen = 255;
-    //           third = 240; 
-    //     }
-    // else 
-    if (val[1] < 555){
+    if (val[1] < 277.5) {
+              myRed = 255;
+              myGreen = 255;
+              third = 240; 
+              first="none";
+              pos="none"
+        }
+    else if (val[1] < 555){
             myRed = 255;
             myGreen = 0;
             third= 0;
+            first="block";
             pos="none"
     }
-    else if(val[1] < 1110)
+    else if(val[1] < 882.5)
     {
             myRed = 0;
             myGreen = 0;
             third = 0; 
-            pos="block"
+            first="block";
+            pos="block"       
     }
-    // else
-    // {
-    //     myRed = 223;
-    //     myGreen = 220;
-    //     third= 217
-    // }
+    else
+    {
+        myRed = 223;
+        myGreen = 220;
+        third= 217
+        first="block";
+        pos="block"  
+    }
   
     theColor = "rgb(" + myRed + "," + myGreen +","+ third + ")"; 
-    return( theColor, pos ); 
+    return(theColor ); 
 }
-// var val = $('#slider').slider("values");
-// console.log(val[0]);
-
-// var pos = $("#slider .ui-slider-range").width();
-// var offset = pos.offsetLeft
-
-// console.log(pos)
-
 
 function refreshSwatch() {
   var slider = $( "#slider" ).slider( "values" ),
   myColor = getTheColor( slider );
-  $( "#slider .statuses__sec" ).css( "display", pos );
+  
   $( "#slider .ui-slider-range" ).css( "background-color", myColor );
   $( "#slider .ui-state-default, .ui-widget-content .ui-state-default" ).css( "border-color", myColor );
+  $( "#slider .statuses__sec" ).css( "display", pos );
+  
+  $( "#slider .statuses__first" ).css( "display", first );
   
     var val = $('#slider').slider("values");
     console.log(val[1]);
