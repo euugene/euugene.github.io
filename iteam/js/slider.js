3d584b7c64b1adf4d53c25ca481ac8d0d5a50153
Eugene <j.gashuck@gmail.com>
2019-11-01 03:08:36 +0200

    sec

--- iteam/js/slider.js
+++ iteam/js/slider.js
@@ -1,4 +1,5 @@
 // if (val[i] < itemWidth * i + 1);
+
 function getTheColor( colorVal ) {
     var theColor = ""; 
      var val = colorVal;
@@ -9,7 +10,9 @@ function getTheColor( colorVal ) {
               third = 240; 
               border="rgb(255, 0, 0)"
               first="none";
-              pos="none"
+              pos="none";
+              
+            thirdpos="none";
             //   bord="block"
             firstBorderColor="rgb(214, 221, 234)"
             secondBorderColor="rgb(214, 221, 234)"
@@ -22,7 +25,8 @@ function getTheColor( colorVal ) {
             border="rgb(255, 0, 0)"
             first="block";
             pos="none";
-            firstBorderColor="rgb(255, 0, 0)"
+            thirdpos="none";
+            firstBorderColor="rgb(255, 0, 0)";
             secondBorderColor="rgb(214, 221, 234)"
             
         // bord="block"
@@ -32,12 +36,13 @@ function getTheColor( colorVal ) {
             myRed = 0;
             myGreen = 0;
             third = 0; 
-            border="rgb(0,0,0)"
+            border="rgb(0,0,0)";
             first="block";
-            pos="block"
-            
-            firstBorderColor="rgb(0,0,0)"
-            secondBorderColor="rgb(0,0,0)"
+            pos="block";
+            thirdpos="none";
+            firstBorderColor="rgb(0,0,0)";
+            secondBorderColor="rgb(0,0,0)";
+
             
         // bord="block"       
     }
@@ -49,32 +54,60 @@ function getTheColor( colorVal ) {
         border="rgb(223,220,214)"
         first="block";
         pos="block";
+        thirdpos="block"
         firstBorderColor="rgb(223,220,214)"
         secondBorderColor="rgb(223,220,214)"
         // bord="block" 
     }
-  
+    
     theColor = "rgb(" + myRed + "," + myGreen +","+ third + ")"; 
     return(theColor ); 
 }
 
+
+       
+
 function refreshSwatch() {
   var slider = $( "#slider" ).slider( "values" ),
   myColor = getTheColor( slider );
+  
 //   $( "#slider .slider__bord" ).css( "display", bord );
   $( "#slider .ui-slider-range" ).css( "background-color", myColor );
   $( "#slider .ui-state-default, .ui-widget-content .ui-state-default" ).css( "border-color", border );
   $( "#slider .slider__bord" ).css( "background", firstBorderColor );
   $( "#slider  .slider__secBord" ).css( "background", secondBorderColor );
+  $( "#slider .statuses__third" ).css( "display", thirdpos );
   $( "#slider .statuses__sec" ).css( "display", pos );
   $( "#slider .statuses__first" ).css( "display", first );
-  
-  
     var val = $('#slider').slider("values");
     console.log(val[1]);
+    
 }
+// var a = document.getElementById('white');
+// a.onclick = function(colorVal) {
+//     // b[1].style.left = "30%";
+//     // var val = colorVal
+// console.log(colorVal)
+//     // b[1]= 
+//     // //производим какие-то действия
+//     // if (this.innerHTML=='On') this.innerHTML = 'Off';
+//     // else this.innerHTML = 'On';
+//     // //предотвращаем переход по ссылке href
+//     // return false;
+    
+// }
+
+$(document).click(function(event) { 
+    $target = $(event.target);
+    if(!$target.closest('#menucontainer').length && 
+    $('#menucontainer').is(":visible")) {
+      $('#menucontainer').hide();
+    }        
+  });
+
 
 $(function() {
+    
     $( "#slider" ).slider({
           orientation: "horizontal",
           range: "min",
@@ -86,3 +119,27 @@ $(function() {
           step : 1,
     });
 });
+
+
+
+// window.onload = function () {
+
+//     //получаем идентификатор элемента
+//     var a = document.getElementById('white');
+    
+//     // var b = document.getElementById('slider');
+//     var b = document.getElementsByClassName("ui-slider-handle ui-state-default ui-corner-all")
+//     console.log(b[1])
+        
+//     //вешаем на него событие
+//     a.onclick = function() {
+//         b[1].style.left = "30%";
+//         // b[1]= 
+//         // //производим какие-то действия
+//         // if (this.innerHTML=='On') this.innerHTML = 'Off';
+//         // else this.innerHTML = 'On';
+//         // //предотвращаем переход по ссылке href
+//         // return false;
+        
+//     }
+// }
