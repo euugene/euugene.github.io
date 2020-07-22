06c5884378653b352b1c5a6b21715a765a4ffe00
Eugene <j.gashuck@gmail.com>
2020-07-22 16:58:58 +0300

    crm

---
+++ crm/src/layouts/EmptyLayout.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class = "grey darken-1 empty-layout">
+    <router-view/>
+  </div>
+
+</template>
+
+<script>
+  import messages from "../utils/messages";
+  export default {
+    computed:{
+      error()  {
+        return this.$store.getters.error //when getters changed, renewed this computed property
+      }
+    },
+    watch: {
+      error(fbError){
+        console.log(fbError)
+        this.$error(messages[fbError.code] || 'Что-то пошло не так')
+      }
+    }
+  }
+</script>
---
+++ crm/src/views/Login.vue
@@ -0,0 +1,98 @@
+<template>
+  <form class="card auth-card" @submit.prevent="handler">
+    <div class="card-content">
+      <span class="card-title">Домашняя бухгалтерия</span>
+      <div class="input-field">
+        <input
+          id="email"
+          type="text"
+          v-model.trim = "email"
+          :class="{invalid: ($v.email.$dirty && !$v.email.required) || ($v.email.$dirty && !$v.email.email)}"
+        >
+        <label for="email">Email</label>
+        <small
+           class="helper-text invalid"
+           v-if ="$v.email.$dirty && !$v.email.required"
+        >Поле Email не должно быть пустым</small>
+        <small
+                class="helper-text invalid"
+                v-if ="$v.email.$dirty && !$v.email.email"
+        >Введите правильный Email</small>
+
+      </div>
+      <div class="input-field">
+        <input
+          id="password"
+          type="password"
+          v-model.trim = "password"
+          :class="{invalid: ($v.password.$dirty && !$v.password.required) || ($v.password.$dirty && !$v.password.minLength)}"
+        >
+        <label for="password">Пароль</label>
+        <small
+          class="helper-text invalid"
+          v-if="$v.password.$dirty && !$v.password.required"
+        >Поле Password не должно быть пустым</small>
+        <small
+                class="helper-text invalid"
+                v-if="$v.password.$dirty && !$v.password.minLength"
+        >Ввеено символов: {{password.length}} из {{$v.password.$params.minLength.min}}</small>
+      </div>
+    </div>
+    <div class="card-action">
+      <div>
+        <button
+          class="btn waves-effect waves-light auth-submit"
+          type="submit"
+        >
+          Войти
+          <i class="material-icons right">send</i>
+        </button>
+      </div>
+
+      <p class="center">
+        Нет аккаунта?
+        <router-link to="/registration">Зарегистрироваться</router-link>
+      </p>
+    </div>
+  </form>
+</template>
+
+<script>
+  import {email, required, minLength} from './../../node_modules/vuelidate/lib/validators'
+  import messages from "../utils/messages";
+  export default {
+    name: 'login',
+    data: () => ({
+      email: '',
+      password: ''
+    }),
+    validations:{
+      email: {email, required},
+      password: {required, minLength: minLength(6)}
+    },
+    mounted() {
+      if(messages[this.$route.query.message]){
+        this.$message(messages[this.$route.query.message])
+      }
+    },
+    methods: {
+      async handler() {
+        if (this.$v.$invalid){
+          this.$v.$touch()
+          return
+        }
+        const formData ={
+          email: this.email,
+          password: this.password
+        }
+        try {
+          await this.$store.dispatch('login', formData)
+          this.$router.push('/')
+        }catch (e) {
+
+        }
+
+      }
+    }
+  }
+</script>
