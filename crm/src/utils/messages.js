bfb01b02ce87deab4ced91dd09b0b2297ac86053
Eugene <j.gashuck@gmail.com>
2020-07-22 17:02:10 +0300

    ready

--- crm/src/utils/messages.js
+++
@@ -1,6 +0,0 @@
-export default {
-    'exit': 'Вы вышли из системы',
-    'auth/user-not-found': 'Введеный email не существует',
-    'auth/wrong-password': 'Вы ввели неправильный пароль',
-    'auth/email-already-in-use': 'Такой email уже существует'
-}
